afd934d930a403152b6d8ffdf75a3cb3b5ffde98
duonght173 <146915480+duonght173@users.noreply.github.com>
2025-06-14 18:23:21 +0700

    chuyen tu dev sang duong

--- routes/web.php
+++ routes/web.php
@@ -51,6 +51,30 @@ Route::prefix('admin')->name('admin.')->group(function () {
     });
 });
 
+//             ==================  CATEGORY =====================
+Route::prefix('listCategory')->name('listCategory.')->group(function () {
+    Route::get('/', [AdminCategoryController::class, 'index'])->name('list');
+
+    Route::get('/detail/{id}', [AdminCategoryController::class, 'show'])->name('detailCategory');
+
+    Route::get('/add', [AdminCategoryController::class, 'create'])->name('addCategory');
+    Route::post('/store', [AdminCategoryController::class, 'store'])->name('storeCategory');
+
+    Route::get('/edit/{id}', [AdminCategoryController::class, 'edit'])->name('editCategory');
+    Route::put('/update{id}', [AdminCategoryController::class, 'update'])->name('updateCategory');
+
+    Route::delete('/delete/{id}', [AdminCategoryController::class, 'destroy'])->name('deleteCategory');
+    Route::get('/search', [AdminCategoryController::class, 'search'])->name('searchCategory');
+});
+
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/brands', [BrandController::class, 'index'])->name('brands.index');
+    Route::get('/brands/create', [BrandController::class, 'create'])->name('brands.create');
+    Route::post('/brands', [BrandController::class, 'store'])->name('brands.store');
+    Route::delete('/brands/{id}', [BrandController::class, 'destroy'])->name('brands.destroy');
+});
+
 
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/brands', [BrandController::class, 'index'])->name('brands.index');
